6d693c5999fe9b9300873a4d7c85a0919ddbc1a0
kirmann <k.dkle@hotmail.fr>
2021-07-09 15:26:31 +0200

    ajouts de trois étoiles + modifs anim

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-const circle = document.querySelector(".circle");
+const circle1 = document.querySelector(".circle1");
+const circle2 = document.querySelector(".circle2");
+const circle3 = document.querySelector(".circle3");
 window.addEventListener("mousemove", (e) => {
-    circle.style.top = e.pageY + "px";
-    circle.style.left = e.pageX + "px";
+    
+    circle1.style.top = e.pageY + "px";
+    circle1.style.left = e.pageX + "px";
+    circle2.style.top = e.pageY  +"px";
+    circle2.style.left = e.pageX  +"px";
+    circle3.style.top = e.pageY  +"px";
+    circle3.style.left = e.pageX  +"px";
+
+
+
+
+
+
+
 })
